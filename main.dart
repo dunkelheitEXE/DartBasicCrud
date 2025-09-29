31c61e3045e3c688694dc778e6ec722ebc686fae
dunkelheitEXE <alangmoreno02@gmail.com>
2025-09-29 01:14:22 -0600

    console line app finished

--- main.dart
+++ main.dart
@@ -8,6 +8,7 @@ void main(List<String> args) {
   print("??       ?? ?    ?    ?   ? ?    ?");
   print("?         ? ???? ????  ???  ??????");
 
+  Manager.initFive();
 
   while(Manager.programStatus) {
       Manager.welcome();
@@ -30,8 +31,12 @@ class Product {
     this.id = Manager.autoIncrement ++;
   }
 
-  void set productDescription(String description) {
+  void productDescription(String description) {
     this.description = description;
+  }
+
+  void productColor(String color) {
+    this.color = color;
   }
 }
 
@@ -44,8 +49,8 @@ class Manager {
     print("Menu Manager:");
     print("1-Create Product");
     print("2-Search an specific product");
-    print("3-Delete Product");
-    print("4-See all current Prodcuts");
+    print("3-See all current products");
+    print("other: exit");
 
     stdout.write("Choose a number:");
     int? intSelection;
@@ -64,8 +69,10 @@ class Manager {
 
     if (intSelection == 1) {
       Manager.createProduct();
-    } else if (intSelection == 4) {
+    } else if (intSelection == 3) {
       Manager.seeAllProducts();
+    } else if (intSelection == 2){
+      Manager.search();
     } else{
       Manager.programStatus = false;
       print("FIN");
@@ -78,7 +85,18 @@ class Manager {
     stdout.write("Enter the price: ");
     String? strPrice = stdin.readLineSync();
     double price = double.parse(strPrice ?? "");
-    Product newProduct = Product("$name", price, "a");
+    stdout.write("Write a Category: ");
+    String? category = stdin.readLineSync();
+    Product newProduct = Product("$name", price, "$category");
+
+    stdout.write("Do you want to set a description (or empty): ");
+    String? description = stdin.readLineSync();
+    stdout.write("Do you want to set a color (or empty): ");
+    String? color = stdin.readLineSync();
+
+    newProduct.productDescription("$description");
+    newProduct.productColor("$color");
+
     list1.add(newProduct);
   }
 
@@ -87,7 +105,9 @@ class Manager {
     for (var item in list1) {
       String? strPrice = item.price.toString();
       String? strId = item.id.toString();
-      String fullItem = "ID: " + strId + " - NAME:" + item.name + " - \$" + strPrice;
+      String description = item.description ?? "";
+      String color = item.color ?? "";
+      String fullItem = "ID: " + strId + " - NAME: " + item.name + " - \$" + strPrice + " - DESCRIPTION: " + description + " - COLOR: " + color;
       print(fullItem);
     }
   }
@@ -96,6 +116,47 @@ class Manager {
     stdout.write("Search: ");
     String? searching = stdin.readLineSync();
     int idSearched = int.parse(searching ?? "");
+    Product? foundProduct = list1.where((product) => product.id == idSearched).firstOrNull;
+    if(foundProduct != null) {
+      print("====================================");
+      String description = foundProduct.description ?? "";
+      String color = foundProduct.color ?? "";
+      String productDesc = "ID: " + foundProduct.id.toString() + " - " + foundProduct.name + " - \$" + foundProduct.price.toString() + " - DESCRIPTION: " + description + " - COLOR: " + color;
+      print(productDesc);
+      print("====================================");
+    } else {
+      print("====================================");
+      print("Not found");
+      print("====================================");
+    }
+  }
+
+  static void initFive() {
+    Product p1 = Product("Laptop", 15000.0, "Electrónica");
+    p1.productDescription("Laptop de alto rendimiento con 16GB RAM");
+    p1.productColor("Gris");
+
+    Product p2 = Product("Smartphone", 8000.0, "Electrónica");
+    p2.productDescription("Pantalla AMOLED 6.5 pulgadas");
+    p2.productColor("Negro");
+
+    Product p3 = Product("Silla Gamer", 3500.0, "Muebles");
+    p3.productDescription("Silla ergonómica con soporte lumbar");
+    p3.productColor("Rojo");
+
+    Product p4 = Product("Reloj Inteligente", 2500.0, "Accesorios");
+    p4.productDescription("Monitor de sueño y ritmo cardíaco");
+    p4.productColor("Blanco");
+
+    Product p5 = Product("Audífonos Bluetooth", 1200.0, "Electrónica");
+    p5.productDescription("Cancelación de ruido activa");
+    p5.productColor("Azul");
+
+    list1.add(p1);
+    list1.add(p2);
+    list1.add(p3);
+    list1.add(p4);
+    list1.add(p5);
   }
 }
 

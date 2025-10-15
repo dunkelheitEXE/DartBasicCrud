72257d4ff7f6f41ae812ef2a13ba91773febfbef
dunkelheitEXE <alangmoreno02@gmail.com>
2025-10-15 16:53:54 -0600

    dev: Learning more abaout Dart & flutter

--- app/app/lib/main.dart
+++ app/app/lib/main.dart
@@ -1,37 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(NombreApp());
+void main() {
+  runApp(MyApp());
+}
 
-class NombreApp extends StatelessWidget {
-  const NombreApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return MaterialApp(
-      title: "Mi primera app",
-      home: Inicio()
-    );
-  }
-}
-
-
-class Inicio extends StatefulWidget {
-  const Inicio({super.key});
-
-  @override
-  State<Inicio> createState() => _InicioState();
-}
-
-class _InicioState extends State<Inicio> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Mi primera App"),
-      ),
-      body: Center(
-        child: Text("Hola Mundo"),
+      home: Scaffold(
+        body: Center(child: Text("Hello, World!"),),
       ),
     );
   }
+  
 }
